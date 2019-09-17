09261487141daf6c40472fe1e24c772985e08ec5
Alec Reid <alecreid0093@gmail.com>
2019-09-17 20:42:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,21 +7,17 @@ class Dog
   def name=(new_name)
     @name = new_name
   end
+  def breed 
+    @breed
+  end
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end
 end
 
 fido = Dog.new 
 fido.name = "Fido"
-
-class Breed 
-  
-  def type 
-    @type
-  end
-  
-  def type=(new_type)
-    @type = new_type
-  end
-end  
 
 snoopy = Breed.new
 snoopy.type = "Beagle"
