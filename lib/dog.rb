f5e912bb4b29f5a97df40f29848cd7ddd10eacd9
Alec Reid <alecreid0093@gmail.com>
2019-09-17 20:43:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,6 +7,7 @@ class Dog
   def name=(new_name)
     @name = new_name
   end
+  
   def breed 
     @breed
   end
@@ -14,6 +15,7 @@ class Dog
   def breed=(new_breed)
     @breed = new_breed
   end
+  
 end
 
 fido = Dog.new 
