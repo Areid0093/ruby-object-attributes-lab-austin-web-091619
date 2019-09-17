aaf90cf7b5bb389df79c7757be484dead0a5ff76
Alec Reid <alecreid0093@gmail.com>
2019-09-17 20:45:48 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,7 +5,17 @@ class Person
   end
   
   def name=(new_name)
-    @name = name
+    @name = new_name
   end
   
+  def job
+    @job
+  end
+  
+  def job=(new_job)
+    @job = new_job
+  end
+end
+
+
   
